c820bf2f592ccb8fc6a445c738a4368b0b6cd666
Tapac <estw272@protonmail.com>
2020-05-26 11:10:05 +0700

    Initial commit

---
+++ apps/labeler/src/labelmaker_version.h
@@ -0,0 +1,10 @@
+#ifndef PROJECT_VERSION_H_239394
+#define PROJECT_VERSION_H_239394
+
+std::string get_labelmaker_version();
+unsigned get_labelmaker_version_major();
+unsigned get_labelmaker_version_minor();
+unsigned get_labelmaker_version_patch();
+unsigned get_labelmaker_version_twear();
+
+#endif //PROJECT_VERSION_H_239394
